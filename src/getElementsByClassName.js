e1742611975ecd25bc90f9f2b7c4733c67adaa5e
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-08-04 11:48:51 -0500

    getElementantsByClassName passing test.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  //Two things:  when to stop?  How to go deeper? - .childNodes? .class
+  var elementArray = []; 
+ 
+	function walk(node, func) {
+	   var children = node.childNodes;
+	   for (var i = 0; i < children.length; i++)  
+     	walk(children[i], func);
+	   	func(node);
+	} 
+
+	walk(document, function(node){
+		if(node.classList){
+			if(node.classList.contains(className)){
+				elementArray.unshift(node)
+			}
+		}
+	})
+	
+  return elementArray
 };
+
+
+
